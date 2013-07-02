708e50e1da862e3d320b7f971f47dcd98064f20b
Moshe <moshe@HP>
2013-07-02 15:51:55 +0300

    Commit 1

---
+++ src/il/ac/courses/students/Students.java
@@ -0,0 +1,18 @@
+package il.ac.courses.students;
+
+public class Students {
+	
+	private String name;
+	private long id;
+	private double avarage;
+	public Students(String name, long id, double avarage) {
+		this.name = name;
+		this.id = id;
+		this.avarage = avarage;
+	}
+	
+	
+	
+	
+
+}
